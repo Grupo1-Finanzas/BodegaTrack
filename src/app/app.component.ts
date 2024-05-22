7faa1fe10e92cc466a501a98d44cffcb7415d01a
DarkArcher822 <u20201c429@upc.edu.pe>
2024-05-22 16:09:20 -0500

    Implementacion del login. No despliega aun, error en el user service para la conexion del fakeapi mediante un archivo.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./public/components/header/header.component";
+import {FooterComponent} from "./public/components/footer/footer.component";
+import {LogInComponent} from "./public/components/log-in/log-in.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FinanzasApp';
+  title = 'BodegaTrack';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LogInComponent} from "./public/components/log-in/log-in.component";
+import {SignUpComponent} from "./public/components/sign-up/sign-up.component";
+import {HomeComponent} from "./bodega-track/components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'log-in', component: LogInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
